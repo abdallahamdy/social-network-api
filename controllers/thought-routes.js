0e89f32e20006a66c24baff848b0ff907387950e
AHamdy <abdulla.mh@hotmail.com>
2022-10-24 13:06:05 -0400

    completed user and thought routes

---
+++ controllers/thought-routes.js
@@ -0,0 +1,50 @@
+const { Thought } = require('../models');
+
+const thoughtController = {
+    // add thought to user
+    addThought({ params, body }, res) {
+        console.log(body);
+        Thought.create(body)
+        .then(({ _id }) => {
+            return Thought.findOneAndUpdate(
+                { _id: params.thoughtId },
+                { $push: { thoughts: _id } },
+                { new: true }
+            );
+        })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id!' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.json(err));
+    },
+  
+    // remove thought
+    removeThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.thoughtId })
+        .then(deletedThought => {
+            if (!deletedThought) {
+                return res.status(404).json({ message: 'No thought with this id!' });
+            }
+            return Thought.findOneAndUpdate(
+                { _id: params.userId },
+                { $pull: { thoughts: params.thoughtId } },
+                { new: true }
+            );
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No user found with this id!' });
+              return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+
+};
+
+module.exports = thoughtController;
--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-api', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
